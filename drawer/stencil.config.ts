2d70036e7cd2f21d64ebe240a10fba461d126ed9
Jan Hein Hoogstad <yeehaa@offcourse.io>
2019-12-17 14:02:13 +0100

    add travis.yml

---
+++ drawer/stencil.config.ts
@@ -0,0 +1,18 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'public-badges-drawer',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ]
+};
