e282c9aa3d669b2784a80490cb7283acd001589d
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-01-28 15:33:33 +0100

    release

--- packages/public-badges-drawer/www/build/publicbadges.esm.js
+++
@@ -1,7 +0,0 @@
-import { p as patchBrowser, b as bootstrapLazy } from './index-a4a9eca1.js';
-import { g as globalScripts } from './app-globals-0f993ce5.js';
-
-patchBrowser().then(options => {
-  globalScripts();
-  return bootstrapLazy([["publicbadges-circle",[[0,"publicbadges-circle",{"badgesCount":[2,"badges-count"],"interactive":[4],"testMode":[4,"test-mode"]}]]],["publicbadges-modal",[[0,"publicbadges-modal",{"theme":[1],"mode":[1],"left":[2],"origin":[1],"badges":[16],"openBadge":[32]}]]],["publicbadges-drawer",[[1,"publicbadges-drawer",{"badgeColor":[1,"badge-color"],"modalTheme":[1,"modal-theme"],"testMode":[4,"test-mode"],"domainName":[1,"domain-name"],"open":[32],"modalMode":[32],"modalLeft":[32],"modalOrigin":[32],"badges":[32]},[[8,"keydown","handleKeyDown"],[0,"closeDrawer","closeDrawer"],[9,"resize","handleWindowResize"]]]]]], options);
-});
